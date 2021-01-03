4d0906d29db86694db978d7eebeb37b31ac15bc8
melissa-24 <melissalongenberger24@gmail.com>
2021-01-03 01:43:05 -0500

    saving changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+import './App.css'
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      {/* Remove everything from this line to line 37 */}
+      <div className="image-container">
+        <img
+          alt="Lambda loves our llamas!"
+          src="https://assets-global.website-files.com/5cd091cfb5499f22bdf72905/5dcda59e63bb6ae5c9282801_small-red-logo.png"
+        ></img>
+      </div>
+      <h2>
+        Welcome <p className="welcome-bottom">Lambda student!</p>
+      </h2>
+      <h4>Your React Application is Ready</h4>
+      <h4>Now go work your magic!</h4>
+      <p className="filler-text">This is filler, no need to keep it!</p>
+      <div className="footer">
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://lambdaschool.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Visit the Lambda Llama home
         </a>
-      </header>
+        <br />
+        <div>
+          <p>Created using Create-React-App</p>
+          <p>Modified and maintained by Lambda School graduate Zac Smith</p>
+          <p>
+            <a
+              href="https://github.com/mrzacsmith"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Github
+            </a>{" "}
+            ::{" "}
+            <a
+              href="https://www.npmjs.com/~mrzacsmith"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              NPM
+            </a>{" "}
+            ::{" "}
+            <a
+              href="https://twitter.com/mrzacsmith"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Twitter
+            </a>
+          </p>
+        </div>
+      </div>
+      {/* Remove everything from line 8 to here */}
     </div>
   );
-}
+};
 
 export default App;
